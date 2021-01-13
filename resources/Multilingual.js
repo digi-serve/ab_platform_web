9d2b82b9f4640c8b76246d5c6fd6d0603c3d8dbf
Johnny Hausman <johnny@hausmanfamily.com>
2021-01-13 17:05:17 -0600

    [add] implement Multilingual.postMissingLabel() feature

--- resources/Multilingual.js
+++ resources/Multilingual.js
@@ -14,12 +14,17 @@ class Multilingual extends MLClass {
 
       this.isAuthenticated = false;
       this._config = null;
+
+      this._missingLabelID = false;
+      this._missingLabels = {};
    }
 
    init(AB) {
       // {ABFactory} AB
 
       this.AB = AB;
+
+      this.labels = this.AB.Config.labelConfig();
 
       // this isn't actually an Async operation, so just resolve()
       return Promise.resolve();
@@ -31,8 +36,26 @@ class Multilingual extends MLClass {
    }
 
    label(key, altText, values = []) {
-      console.error("Multilingual:label(): Implement Label Storage.");
-      var label = altText;
+      // NOTE: transition to new Labels
+      // currently our code still uses the L(key, altText, values) format, but
+      // the labels we get back are in L(altText, values) format.
+      //
+      // we are going to keep this like this until we have a well functioning
+      // UI display of our Apps, which should flesh out most of the missing
+      // labels to our files.
+      //
+      // After that, convert our L(k, t, v) to L(t, v)
+
+      var newKey = altText;
+      if (newKey[0] == "*") {
+         newKey = newKey.slice(1);
+      }
+      var label = this.labels[newKey];
+      if (!label) {
+         this.postMissingLabel(key, altText);
+         label = altText;
+      }
+
       values.forEach((v, i) => {
          var sub = `{${i}}`;
          label = label.replaceAll(sub, v);
@@ -47,6 +70,41 @@ class Multilingual extends MLClass {
       return [];
    }
 
+   postMissingLabel(key, altText) {
+      if (this._missingLabelID) {
+         clearTimeout(this._missingLabelID);
+      }
+      var batchLength = 50;
+      var sendBatch = () => {
+         var batchLabels = [];
+         let missingLabels = Object.keys(this._missingLabels).map(
+            (k) => this._missingLabels[k]
+         );
+         if (missingLabels.length <= batchLength) {
+            batchLabels = missingLabels;
+            this._missingLabels = {};
+         } else {
+            for (var i = 0; i <= batchLength; i++) {
+               var l = missingLabels.shift();
+               batchLabels.push(l);
+               delete this._missingLabels[l.key];
+            }
+         }
+         this.AB.Network.post({
+            url: "/multilingual/label-missing",
+            data: { labels: JSON.stringify(batchLabels) },
+         }).then(() => {
+            if (Object.keys(this._missingLabels).length > 0) {
+               sendBatch();
+            }
+         });
+      };
+      this._missingLabelID = setTimeout(() => {
+         sendBatch();
+      }, 1000);
+
+      this._missingLabels[key] = { key, altText };
+   }
    ///
    /// Disable un-needed ABMLClass functionality
    ///
