8a1f0dd3cd016c84789b140eb4e8016745537524
wongpratan <wongpratan@hotmail.com>
2021-04-02 16:02:31 +0700

    -Remove the calculate field option from the formula field panel

--- AppBuilder/platform/dataFields/ABFieldFormula.js
+++ AppBuilder/platform/dataFields/ABFieldFormula.js
@@ -96,8 +96,9 @@ var ABFieldFormulaComponent = new ABFieldComponent({
 
             objLink.fields().forEach((fLink) => {
                // pull 'number' and 'calculate' fields from link objects
-               if (fLink.key == "number" || fLink.key == "calculate") {
-                  // if (fLink.key == 'number') { // NOTE: calculate fields does not support in queries
+               // if (fLink.key == "number" || fLink.key == "calculate") {
+               // NOTE: calculate fields does not support in queries
+               if (fLink.key == 'number') { // NOTE: calculate fields does not support in queries
                   options.push({
                      // UUID:UUID
                      id: `${f.id}:${fLink.id}`,
