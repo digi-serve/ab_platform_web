badc1e37bd2613716a61f2fdce927dcfeeba9d32
wongpratan <wongpratan@hotmail.com>
2021-09-01 15:48:54 +0700

    Migrate V1 > V2 From: https://github.com/appdevdesigns/app_builder/commit/919cac659

--- AppBuilder/platform/RowFilter.js
+++ AppBuilder/platform/RowFilter.js
@@ -1373,7 +1373,7 @@ module.exports = class RowFilter extends RowFilterCore {
             $viewConditionValue &&
             $viewConditionValue.getChildViews()
          ) {
-            var vals = f.value.split(":");
+            var vals = (f.value || "").split(":");
             var index = 0;
             $viewConditionValue.getChildViews().forEach((element) => {
                $$(element).blockEvent();
