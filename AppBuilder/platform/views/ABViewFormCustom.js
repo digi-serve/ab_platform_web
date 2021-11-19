1f8445fd8a02d3cfb152bf5286d9034c0523c29b
nate <7259@pm.me>
2021-11-19 09:37:54 +0700

    add data-cy to file/image form fields

--- AppBuilder/platform/views/ABViewFormCustom.js
+++ AppBuilder/platform/views/ABViewFormCustom.js
@@ -170,6 +170,10 @@ module.exports = class ABViewFormCustom extends ABViewFormCustomCore {
          var rowData = {},
             node = elem.$view;
 
+         // Add data-cy attributes
+         const dataCy = `${this.key} ${field.key} ${field.columnName} ${this.id} ${this.parent.id}`;
+         node.querySelector(`.ab-${field.key}-data-field`).setAttribute('data-cy', dataCy);
+
          let options = {
             formId: ids.component,
             editable: this.settings.disable == 1 ? false : true,
