65996935df54935b5a0f349acac806dce319ecbb
James Duncan <james@digiserve.org>
2022-01-18 11:40:14 +0700

    fix dataview layout issues

--- AppBuilder/platform/views/ABViewDataview.js
+++ AppBuilder/platform/views/ABViewDataview.js
@@ -297,9 +297,9 @@ module.exports = class ABViewDataview extends ABViewDataviewCore {
          var flexlayout = {
             id: ids.dataFlexView,
             view: "flexlayout",
-            paddingX: 15,
-            paddingY: 19,
-            type: "space",
+            type: "clean",
+            padding: 10,
+            css: { background: "#ebedf0 !important" },
             cols: [],
          };
          webix.ui(flexlayout, $$(ids.scrollview), $$(ids.dataFlexView));
