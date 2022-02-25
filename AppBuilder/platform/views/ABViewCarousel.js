f0091dd1b410f2dd60df923c03ee4df1a2a0b760
wongpraatan <wongpratan@hotmail.com>
2022-02-25 10:57:06 +0700

    +Add loading cursor to the carousel widget - https://github.com/appdevdesigns/app_builder/commit/974e520c

--- AppBuilder/platform/views/ABViewCarousel.js
+++ AppBuilder/platform/views/ABViewCarousel.js
@@ -454,6 +454,12 @@ export default class ABViewCarousel extends ABViewCarouselCore {
          dv.on("create", () => {
             dv.reloadData();
          });
+         dv.on("initializingData", () => {
+            _logic.busy();
+         });
+         dv.on("initializedData", () => {
+            _logic.ready();
+         });
 
          if (this.settings.filterByCursor) {
             dv.on("changeCursor", () => {
