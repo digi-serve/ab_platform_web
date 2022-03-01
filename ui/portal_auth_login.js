b691b4921c0176020412dc73a7cf6b62ed058375
Johnny Hausman <johnny@hausmanfamily.com>
2022-03-01 11:16:04 +0700

    [add] Feature:  Forgot Password authentication case now implemented.

--- ui/portal_auth_login.js
+++ ui/portal_auth_login.js
@@ -1,4 +1,7 @@
 import ClassUI from "./ClassUI.js";
+import PortalAuthLoginForm from "./portal_auth_login_form.js";
+import PortalAuthLoginResetPassword from "./portal_auth_login_resetPassword.js";
+import PortalAuthLoginResetRequest from "./portal_auth_login_resetRequest.js";
 
 class PortalAuthLogin extends ClassUI {
    constructor() {
@@ -10,220 +13,60 @@ class PortalAuthLogin extends ClassUI {
 
       return {
          id: "portal_auth_login",
-         css: "portalLogin",
-         cols: [
-            {},
-            {
-               width: "300",
-               css: "portalLoginForm",
-               rows: [
-                  {},
-                  {
-                     template:
-                        "<div style='text-align: center; font-size:160px; line-height: 160px;'><i style='background-color: #666; color: transparent; text-shadow: 0px 1px 1px rgba(255,255,255,0.5); -webkit-background-clip: text; -moz-background-clip: text; background-clip: text;' class='fa fa-user-circle-o'></i></div>",
-                     borderless: true,
-                     height: 190,
-                     type: "clean",
-                  },
-                  {
-                     id: "portal_auth_login_form",
-                     view: "form",
-                     type: "clean",
-                     css: { background: "transparent !important" },
-                     borderless: true,
-                     elementsConfig: {
-                        bottomPadding: 20,
-                        height: 52,
-                     },
-                     elements: [
-                        //// TODO: James make this look pretty ...
-                        {
-                           id: "portal_auth_login_form_tenantList",
-                           view: "select",
-                           // label: "Tenant",
-                           name: "tenant",
-                           attributes: {
-                              "data-cy": "portal_auth_login_form_tenantList",
-                           },
-                           value: 1,
-                           options: [
-                              { id: 1, value: "Master" },
-                              { id: 2, value: "Release" },
-                           ],
-                        },
-                        {
-                           view: "text",
-                           placeholder: "Email",
-                           name: "email",
-                           id: "email",
-                           attributes: {
-                              "data-cy": "portal_auth_login_form_email",
-                           },
-                           // required: true,
-                           validate: webix.rules.isEmail,
-                           invalidMessage: "Please enter a valid email.",
-                           validateEvent: "blur",
-                        },
-                        {
-                           view: "text",
-                           type: "password",
-                           placeholder: "Password",
-                           name: "password",
-                           attributes: {
-                              "data-cy": "portal_auth_login_form_password",
-                           },
-                           // required: true,
-                           validate: webix.rules.isNotEmpty,
-                           invalidMessage: "Please enter your password.",
-                           validateEvent: "blur",
-                        },
-                        {
-                           margin: 10,
-                           paddingX: 2,
-                           borderless: true,
-                           cols: [
-                              {},
-                              {
-                                 view: "button",
-                                 label: "Login",
-                                 type: "form",
-                                 id: "portal_auth_login_form_submit",
-                                 css: "webix_primary",
-                                 width: 150,
-                                 hotkey: "enter",
-                                 click() {
-                                    var form = $$("portal_auth_login_form");
-                                    if (form.validate()) {
-                                       var values = form.getValues();
-                                       self.error(); // hids the error message
-
-                                       // this.AB.Network.post()
-                                       // can either be a Relay or Rest operation.
-                                       // we should get the response from the
-                                       // published JobRequest initialized in
-                                       // the .init() routine.
-                                       self.AB.Network.post(
-                                          { url: "/auth/login", data: values },
-                                          {
-                                             key:
-                                                "portal_auth_login" /*, context:{} */,
-                                          }
-                                       ).catch((err) => {
-                                          console.log(err);
-                                       });
-                                    }
-                                 },
-                                 on: {
-                                    onAfterRender() {
-                                       ClassUI.CYPRESS_REF(this);
-                                    },
-                                 },
-                              },
-                              {},
-                           ],
-                        },
-                        {
-                           id: "portal_auth_login_form_errormsg",
-                           view: "template",
-                           css: "webix_control",
-                           height: 32,
-                           on: {
-                              onAfterRender() {
-                                 ClassUI.CYPRESS_REF(this);
-                              },
-                           },
-                        },
-                     ],
-                  },
-                  {},
-               ],
-            },
-            {},
+         view: "multiview",
+         animate: false,
+         rows: [
+            PortalAuthLoginForm.ui(),
+            PortalAuthLoginResetPassword.ui(),
+            PortalAuthLoginResetRequest.ui(),
          ],
       };
    }
 
-   error(message) {
-      if (message) {
-         $$("portal_auth_login_form_errormsg").setHTML(
-            `<div class='webix_invalid'><div class='webix_inp_bottom_label'><center>${message}</center></div></div>`
-         );
-         $$("portal_auth_login_form_errormsg").show();
-      } else {
-         $$("portal_auth_login_form_errormsg").hide();
-      }
-   }
-
-   init(AB) {
+   async init(AB) {
       this.AB = AB;
-      var siteConfig = this.AB.Config.siteConfig();
-      if (siteConfig) {
-         // replace options in tenant list with siteConfig.tenants:
-         var newOptions = [];
-         siteConfig.tenants.forEach((t) => {
-            var opt = {
-               id: t.uuid,
-               value: t.title || t.key,
-            };
-            newOptions.push(opt);
-         });
-         // console.log("newOptions:", newOptions);
 
-         $$("portal_auth_login_form_tenantList").define("options", newOptions);
-      }
-
-      var tID = this.AB.Tenant.id();
-      if (tID) {
-         $$("portal_auth_login_form_tenantList").define("value", tID);
-      }
-
-      this.error(); // hides the default error message.
-
-      this.AB.Network.on("portal_auth_login", (context, err, response) => {
-         // Listen for our login responses:
-         // console.log("Network.on():", context, err, response);
-
-         if (err) {
-            // any http 400-500 response should show up here:
-            if (err.code) {
-               switch (err.code) {
-                  case "EINVALIDLOGIN":
-                     this.error(err.message);
-                     break;
-
-                  case "EFAILEDATTEMPTS":
-                     this.error(err.message);
-                     $$("portal_auth_login_form_submit").hide();
-                     break;
-
-                  default:
-                     this.AB.error(err);
-                     break;
-               }
-            }
-            return;
-         }
-
-         if (
-            response.user ||
-            (response.status == "success" && response.data.user)
-         ) {
-            // we can .reload() from cache given that all our
-            // dynamic data comes from our initial /config call
-            window.location.reload(false);
-         } else {
-            if (response.status == "error") {
-               console.log("what to do with this error:");
-               console.error(response);
-            }
-         }
+      var allInits = [];
+      allInits.push(PortalAuthLoginForm.init(AB));
+      PortalAuthLoginForm.on("request.reset", () => {
+         PortalAuthLoginResetRequest.show();
       });
 
-      return Promise.resolve();
+      allInits.push(PortalAuthLoginResetRequest.init(AB));
+      PortalAuthLoginResetRequest.on("login", () => {
+         PortalAuthLoginForm.show();
+      });
+
+      allInits.push(PortalAuthLoginResetPassword.init(AB));
+      PortalAuthLoginResetPassword.on("updated", () => {
+         console.log(">>>> Password Changed ... NOW WHAT? <<<<<<");
+      });
+
+      await Promise.all(allInits);
    }
 
-   show() {
-      $$("portal_auth_login").show();
+   show(defaultView) {
+      // defaultView should be:
+      //   "auth_login_form",
+      //   "auth_login_passwordReset"
+
+      if (defaultView) {
+         var view = defaultView.split("_").pop();
+         switch (view) {
+            case "resetPassword":
+               PortalAuthLoginResetPassword.show();
+               break;
+
+            default:
+            case "form":
+               PortalAuthLoginForm.show();
+               break;
+         }
+         return;
+      }
+
+      // Default to the LoginForm
+      PortalAuthLoginForm.show();
    }
 }
 
---
+++ ui/portal_auth_login_resetRequest.js
@@ -0,0 +1,226 @@
+import ClassUI from "./ClassUI.js";
+
+var L = null;
+
+class PortalAuthLoginResetRequest extends ClassUI {
+   constructor() {
+      super();
+   }
+
+   ui() {
+      var self = this;
+
+      if (!this.AB) {
+         // we need to init() before being able to translate our UI:
+         return {
+            id: "portal_auth_login_reset_request",
+         };
+      }
+
+      return {
+         id: "portal_auth_login_reset_request",
+         css: "portalLogin",
+         cols: [
+            {},
+            {
+               width: "300",
+               id: "password_reset_email",
+               css: "portalLoginForm",
+               rows: [
+                  {},
+                  {
+                     template:
+                        "<div style='text-align: center; font-size:160px; line-height: 160px;'><i style='background-color: #666; color: transparent; text-shadow: 0px 1px 1px rgba(255,255,255,0.5); -webkit-background-clip: text; -moz-background-clip: text; background-clip: text;' class='fa fa-envelope-o'></i></div>",
+                     borderless: true,
+                     height: 190,
+                     type: "clean",
+                  },
+                  {
+                     id: "portal_reset_password_form",
+                     view: "form",
+                     type: "clean",
+                     css: { background: "transparent !important" },
+                     borderless: true,
+                     elementsConfig: {
+                        bottomPadding: 20,
+                     },
+                     elements: [
+                        {
+                           template: `<p>${L(
+                              "Enter your email. We'll send a link to reset your password."
+                           )}</p>`,
+                           css: {
+                              background: "transparent",
+                              border: "none",
+                              "font-size": "16px",
+                              color: "black",
+                              "text-align": "center",
+                           },
+                           height: 80,
+                        },
+                        {
+                           height: 72,
+                           borderless: true,
+                           cols: [
+                              {
+                                 id: "reset_tenantList",
+                                 view: "select",
+                                 // label: "Tenant",
+                                 name: "tenant",
+                                 attributes: {
+                                    "data-cy": "reset_tenantList",
+                                 },
+                                 value: 1,
+                                 options: [
+                                    { id: 1, value: "Master" },
+                                    { id: 2, value: "Release" },
+                                 ],
+                              },
+                              {
+                                 id: "reset-email",
+                                 view: "text",
+                                 placeholder: L("Email"),
+                                 name: "email",
+                                 validate: webix.rules.isEmail,
+                                 invalidMessage: L(
+                                    "Please enter a valid email."
+                                 ),
+                                 validateEvent: "blur",
+                              },
+                              {
+                                 rows: [
+                                    {
+                                       view: "button",
+                                       height: 52,
+                                       icon: "fa fa-paper-plane",
+                                       type: "icon",
+                                       css: "webix_primary",
+                                       autowidth: true,
+                                       hotkey: "enter",
+                                       click() {
+                                          var email = $$("reset-email");
+                                          if (email.validate()) {
+                                             email = email.getValue();
+                                             var tenant = $$(
+                                                "reset_tenantList"
+                                             ).getValue();
+                                             self.AB.Network.post(
+                                                {
+                                                   url: "/auth/login/reset",
+                                                   data: {
+                                                      email,
+                                                      tenant,
+                                                      url:
+                                                         window.location
+                                                            .origin ||
+                                                         window.location.href,
+                                                   },
+                                                },
+                                                {
+                                                   key:
+                                                      "portal_auth_login_reset",
+                                                   context: { email },
+                                                }
+                                             ).catch((err) => {
+                                                console.log(err);
+                                             });
+                                          }
+                                       },
+                                    },
+                                    {},
+                                 ],
+                              },
+                           ],
+                        },
+                        {
+                           view: "button",
+                           label: L("Back to Sign In Page"),
+                           css: "webix_transparent",
+                           click() {
+                              self.emit("login");
+                           },
+                        },
+                        {
+                           id: "portal_auth_login_form_errormsg",
+                           view: "template",
+                           css: "webix_control",
+                           height: 32,
+                        },
+                     ],
+                  },
+                  {},
+               ],
+            },
+            {},
+         ],
+      };
+   }
+
+   async init(AB) {
+      this.AB = AB;
+
+      L = (...params) => {
+         return this.AB.Multilingual.label(...params);
+      };
+
+      // now replace our initial placeholder with our viewable form
+      webix.ui(this.ui(), $$("portal_auth_login_reset_request"));
+
+      var $tenant = $$("reset_tenantList");
+      var siteConfig = this.AB.Config.siteConfig();
+      if (siteConfig) {
+         // replace options in tenant list with siteConfig.tenants:
+         var newOptions = [];
+         siteConfig.tenants.forEach((t) => {
+            var opt = {
+               id: t.uuid,
+               value: t.title || t.key,
+            };
+            newOptions.push(opt);
+         });
+
+         $tenant.define("options", newOptions);
+      }
+
+      var tID = this.AB.Tenant.id();
+      if (tID) {
+         $tenant.define("value", tID);
+
+         // if the tID matches one of our options, then we can hide
+         // the input.
+         // eg: they entered a route [tenant].our.url
+         // we simple auto select the tenant and don't show this component.
+         var opt = newOptions.find((o) => o.id == tID);
+         if (opt) {
+            $tenant.hide();
+         }
+      }
+
+      this.AB.Network.on(
+         "portal_auth_login_reset",
+         (context, err, response) => {
+            // Listen for our responses:
+            // Show a popup for confirmation that an email was sent.
+            // context.email : {string} entered email address.
+
+            var text = L(
+               "<p>An email with instructions on how to reset your password has been sent to <b>{0}</b>.</p><p>Check your spam or junk folder if you don’t see the email in your inbox.</p>",
+               [context.email]
+            );
+
+            webix.alert({
+               title: L("Email Sent"),
+               ok: L("Okay"),
+               width: 500,
+               text: text,
+            });
+         }
+      );
+   }
+
+   show() {
+      $$("portal_auth_login_reset_request").show();
+   }
+}
+
+export default new PortalAuthLoginResetRequest();
