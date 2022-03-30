ea1c9e4b4a58bf04da6749896fac0198a87a7852
James Duncan <james@digiserve.org>
2022-03-30 15:04:39 +0700

    login ui update and submodules update

--- ui/portal_auth_login_resetPassword.js
+++ ui/portal_auth_login_resetPassword.js
@@ -23,109 +23,127 @@ class PortalAuthLoginResetPassword extends ClassUI {
          cols: [
             {},
             {
-               id: "portal_auth_login_reset_password_col",
-               width: "300",
-               css: "portalLoginForm",
                rows: [
                   {},
                   {
-                     template:
-                        "<div style='text-align: center; font-size:160px; line-height: 160px;'><i style='background-color: #666; color: transparent; text-shadow: 0px 1px 1px rgba(255,255,255,0.5); -webkit-background-clip: text; -moz-background-clip: text; background-clip: text;' class='fa fa-user-circle-o'></i></div>",
-                     borderless: true,
-                     height: 190,
-                     type: "clean",
-                  },
-                  {
-                     id: "portal_auth_login_reset_password_form",
-                     view: "form",
-                     type: "clean",
-                     css: { background: "transparent !important" },
-                     borderless: true,
-                     elementsConfig: {
-                        height: 52,
-                     },
-                     elements: [
+                     width: 360,
+                     rows: [
+                        {},
                         {
-                           id: "new-password-email",
-                           template: ` ${this.AB.Account.email()} `,
-                           css: {
-                              background: "transparent !important",
-                              border: "none",
-                              "font-size": "16px",
-                              color: "black",
-                              "text-align": "center",
-                           },
-                        },
-                        {
-                           id: "password-new",
-                           view: "text",
-                           type: "password",
-                           placeholder: L("Enter a new Password"),
-                           name: L("Enter a new Password"),
-                           validate: (v) => {
-                              return v.length >= 8;
-                           },
-                           invalidMessage: L(
-                              "Needs to be at least 8 characters"
-                           ),
-                           validateEvent: "blur",
-                           bottomPadding: 20,
-                        },
-                        {
-                           id: "password-confirm",
-                           view: "text",
-                           type: "password",
-                           placeholder: L("Re-enter Password"),
-                           name: "email",
-                           validate: (v) => {
-                              return v === $$("password-new").getValue();
-                           },
-                           invalidMessage: L("Passwords do not match"),
-                           validateEvent: "blur",
-                           bottomPadding: 20,
-                        },
-                        {
-                           margin: 10,
-                           paddingX: 2,
-                           borderless: true,
-                           cols: [
-                              {},
+                           id: "portal_auth_login_reset_password_col",
+                           css: "portalLoginForm",
+                           padding: 30,
+                           rows: [
                               {
-                                 view: "button",
-                                 label: L("Save"),
-                                 type: "form",
-                                 css: "webix_primary",
-                                 width: 150,
-                                 hotkey: "enter",
-                                 click: () => {
-                                    if (
-                                       $$(
-                                          "portal_auth_login_reset_password_form"
-                                       ).validate()
-                                    ) {
-                                       let password = $$(
-                                          "password-new"
-                                       ).getValue();
-                                       this.AB.Network.post(
+                                 template:
+                                    "<div style='text-align: center; font-size:160px; line-height: 160px;'><i style='background-color: #666; color: transparent; text-shadow: 0px 1px 1px rgba(255,255,255,0.5); -webkit-background-clip: text; -moz-background-clip: text; background-clip: text;' class='fa fa-user-circle-o'></i></div>",
+                                 borderless: true,
+                                 height: 190,
+                                 type: "clean",
+                              },
+                              {
+                                 id: "portal_auth_login_reset_password_form",
+                                 view: "form",
+                                 type: "clean",
+                                 css: { background: "transparent !important" },
+                                 borderless: true,
+                                 elementsConfig: {
+                                    height: 52,
+                                 },
+                                 elements: [
+                                    {
+                                       id: "new-password-email",
+                                       template: ` ${this.AB.Account.email()} `,
+                                       css: {
+                                          background: "transparent !important",
+                                          border: "none",
+                                          "font-size": "16px",
+                                          color: "black",
+                                          "text-align": "center",
+                                       },
+                                    },
+                                    {
+                                       id: "password-new",
+                                       view: "text",
+                                       type: "password",
+                                       placeholder: L("Enter a new Password"),
+                                       name: L("Enter a new Password"),
+                                       validate: (v) => {
+                                          return v.length >= 8;
+                                       },
+                                       invalidMessage: L(
+                                          "Needs to be at least 8 characters"
+                                       ),
+                                       validateEvent: "blur",
+                                       bottomPadding: 20,
+                                    },
+                                    {
+                                       id: "password-confirm",
+                                       view: "text",
+                                       type: "password",
+                                       placeholder: L("Re-enter Password"),
+                                       name: "email",
+                                       validate: (v) => {
+                                          return (
+                                             v === $$("password-new").getValue()
+                                          );
+                                       },
+                                       invalidMessage: L(
+                                          "Passwords do not match"
+                                       ),
+                                       validateEvent: "blur",
+                                       bottomPadding: 20,
+                                    },
+                                    {
+                                       margin: 10,
+                                       paddingX: 2,
+                                       borderless: true,
+                                       cols: [
+                                          {},
                                           {
-                                             url: "/auth/password/reset",
-                                             data: {
-                                                password,
+                                             view: "button",
+                                             label: L("Save"),
+                                             type: "form",
+                                             css: "webix_primary",
+                                             width: 150,
+                                             hotkey: "enter",
+                                             click: () => {
+                                                if (
+                                                   $$(
+                                                      "portal_auth_login_reset_password_form"
+                                                   ).validate()
+                                                ) {
+                                                   let password = $$(
+                                                      "password-new"
+                                                   ).getValue();
+                                                   this.AB.Network.post(
+                                                      {
+                                                         url:
+                                                            "/auth/password/reset",
+                                                         data: {
+                                                            password,
+                                                         },
+                                                      },
+                                                      {
+                                                         key:
+                                                            "portal_auth_password_reset",
+                                                         context: {},
+                                                      }
+                                                   ).catch((err) => {
+                                                      console.log(err);
+                                                   });
+                                                }
                                              },
                                           },
-                                          {
-                                             key: "portal_auth_password_reset",
-                                             context: {},
-                                          }
-                                       ).catch((err) => {
-                                          console.log(err);
-                                       });
-                                    }
-                                 },
+                                          {},
+                                       ],
+                                    },
+                                 ],
                               },
-                              {},
                            ],
                         },
+
+                        {},
                      ],
                   },
                   {},
