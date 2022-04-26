27db71cb919f37e20b4de5e4bbfd1f440f05d3ca
guyyoo <guy.konlawat@digiserve.org>
2022-04-26 12:29:18 +0700

    Fix services task's object ABProcessTaskService ABProcessTaskServiceGetResetPasswordUrl ABProcessTaskServiceInsertRecord and ABProcessTaskServiceQuery

---
+++ AppBuilder/platform/process/tasks/ABProcessTaskServiceGetResetPasswordUrl.js
@@ -0,0 +1,7 @@
+// import ABApplication from "./ABApplication"
+// const ABApplication = require("./ABApplication"); // NOTE: change to require()
+const ABProcessTaskServiceGetResetPasswordUrlCore = require("../../../core/process/tasks/ABProcessTaskServiceGetResetPasswordUrlCore.js");
+
+module.exports = class ABProcessTaskServiceGetResetPasswordUrl extends (
+   ABProcessTaskServiceGetResetPasswordUrlCore
+) {};
