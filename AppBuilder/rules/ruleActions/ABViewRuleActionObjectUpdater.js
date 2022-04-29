9ba5ebb5a27a73ec7ee40683528ca605c8fd7d8c
nate <7259@pm.me>
2022-04-29 16:24:52 +0700

    Revert "Fix record rule insert based on cursor."
    This reverts commit 6ce207682fd77132adbc4b6c82bb15a20646604b.

--- AppBuilder/rules/ruleActions/ABViewRuleActionObjectUpdater.js
+++ AppBuilder/rules/ruleActions/ABViewRuleActionObjectUpdater.js
@@ -942,8 +942,6 @@ module.exports = class ABViewRuleActionObjectUpdater extends ABViewRuleAction {
             var filterConditions = this.currentForm.AB.cloneDeep(
                op.filterConditions
             );
-            // The cloned dataCollection is not copying the current cursor, so save it here
-            const cursor = dataCollection.getCursor();
             if (filterConditions && filterConditions.rules) {
                filterConditions.rules
                   .filter((r) => {
@@ -980,7 +978,7 @@ module.exports = class ABViewRuleActionObjectUpdater extends ABViewRuleAction {
                // TODO: rename to 'select-cursor'
                case "select-one":
                default:
-                  value = cursor; // dataView.getItem(dataView.getCursor());
+                  value = clonedDataCollection.getCursor(); // dataView.getItem(dataView.getCursor());
 
                   if (value) {
                      // NOTE: webix documentation issue: .getCursor() is supposed to return
