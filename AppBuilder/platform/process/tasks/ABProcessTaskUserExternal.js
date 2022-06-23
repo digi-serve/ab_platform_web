75a02226174ac4ccadc83c7d12309c871fd6ff63
guyyoo <guy.konlawat@digiserve.org>
2022-06-23 11:48:26 +0700

    Add the new user task "External"

---
+++ AppBuilder/platform/process/tasks/ABProcessTaskUserExternal.js
@@ -0,0 +1,7 @@
+// import ABApplication from "./ABApplication"
+// const ABApplication = require("./ABApplication"); // NOTE: change to require()
+const ABProcessTaskUserExternalCore = require("../../../core/process/tasks/ABProcessTaskUserExternalCore.js");
+
+module.exports = class ABProcessTaskUserExternal extends (
+   ABProcessTaskUserExternalCore
+) {};
