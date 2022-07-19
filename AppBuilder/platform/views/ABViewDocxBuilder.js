f232b382111a1c5df2802b14520ed291cfc7b84e
wongpraatan <wongpratan@hotmail.com>
2022-07-19 13:21:02 +0700

    +Migrate DOCX Builder widget - https://github.com/digi-serve/plugin_ABDesigner/issues/47 https://github.com/appdevdesigns/app_builder/blob/master/assets/opstools/AppBuilder/classes/platform/views/ABViewDocxBuilder.js

--- AppBuilder/platform/views/ABViewDocxBuilder.js
+++ AppBuilder/platform/views/ABViewDocxBuilder.js
@@ -1,4 +1,5 @@
 const ABViewDocxBuilderCore = require("../../core/views/ABViewDocxBuilderCore");
+const ABViewDocxBuilderComponent = require("./viewComponent/ABViewDocxBuilderComponent");
 
 const ABFieldConnect = require("../dataFields/ABFieldConnect");
 const ABFieldImage = require("../dataFields/ABFieldImage");
@@ -11,371 +12,62 @@ const JSZip = require("../../../js/jszip.min.js");
 
 const ABViewDocxBuilderPropertyComponentDefaults = ABViewDocxBuilderCore.defaultValues();
 
-let L = (...params) => AB.Multilingual.label(...params);
-
-function letUserDownload(blob, filename) {
-   let url = window.URL.createObjectURL(blob);
-
-   let a = document.createElement("a");
-   a.href = url;
-   a.download = filename;
-   document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
-   a.click();
-   a.remove(); //afterwards we remove the element again
-
-   window.URL.revokeObjectURL(url);
-}
+const L = (...params) => AB.Multilingual.label(...params);
 
 module.exports = class ABViewDocxBuilder extends ABViewDocxBuilderCore {
-   // constructor(values, application, parent, defaultValues) {
-   //    super(values, application, parent, defaultValues);
-   // }
-
-   //
-   //	Editor Related
-   //
-
-   /**
-    * @method editorComponent
-    * return the Editor for this UI component.
-    * the editor should display either a "block" view or "preview" of
-    * the current layout of the view.
-    * @param {string} mode what mode are we in ['block', 'preview']
-    * @return {Component}
-    */
-   editorComponent(App, mode) {
-      var idBase = "ABViewDocxBuilderEditorComponent";
-
-      var DocxBuilderComponent = this.component(App, idBase);
-
-      return DocxBuilderComponent;
-   }
-
-   //
-   // Property Editor
-   //
-   static propertyEditorDefaultElements(App, ids, _logic, ObjectDefaults) {
-      var commonUI = super.propertyEditorDefaultElements(
-         App,
-         ids,
-         _logic,
-         ObjectDefaults
-      );
-
-      _logic.validateType = (item) => {
-         // verify file type
-         var acceptableTypes = ["docx"];
-         var type = item.type.toLowerCase();
-         if (acceptableTypes.indexOf(type) == -1) {
-            //// TODO: multilingual
-            webix.message(
-               "Only [" + acceptableTypes.join(", ") + "] files are supported"
-            );
-            return false;
-         } else {
-            // set upload url to uploader
-            let currView = _logic.currentEditObject();
-            let uploadUrl = currView.uploadUrl();
-
-            $$(ids.docxFile).define("upload", uploadUrl);
-            $$(ids.docxFile).refresh();
-
-            return true;
-         }
-      };
-
-      _logic.uploadedFile = (fileInfo) => {
-         if (!fileInfo || !fileInfo.data) return;
-
-         let currView = _logic.currentEditObject();
-         currView.settings.filename = fileInfo.data.uuid;
-         currView.filelabel = fileInfo.name;
-
-         $$(ids.filelabel).setValue(currView.filelabel);
-         $$(ids.docxDownload).show();
-      };
-
-      _logic.downloadFile = () => {
-         let currView = _logic.currentEditObject();
-         let url = currView.downloadUrl();
-
-         fetch(url)
-            .then((response) => response.blob())
-            .then((blob) => {
-               letUserDownload(blob, currView.filelabel);
-            });
-      };
-
-      // Populate language options
-      var langOptions = App.AB.Multilingual.languages().map((lang) => {
-         return {
-            id: lang.language_code,
-            value: lang.language_label,
-         };
-      });
-
-      $$(ids.language).define("options", langOptions);
-      $$(ids.language).refresh();
-
-      // in addition to the common .label  values, we
-      // ask for:
-      return commonUI.concat([
-         {
-            view: "fieldset",
-            label: L("Data:"),
-            labelWidth: this.AB.UISettings.config().labelWidthLarge,
-            body: {
-               type: "clean",
-               padding: 10,
-               rows: [
-                  {
-                     name: "datacollection",
-                     // view: 'richselect',
-                     view: "multicombo",
-                     label: L("Data Source"),
-                     labelWidth: this.AB.UISettings.config().labelWidthLarge,
-                  },
-               ],
-            },
-         },
-
-         {
-            view: "fieldset",
-            label: L("Template file:"),
-            labelWidth: this.AB.UISettings.config().labelWidthLarge,
-            body: {
-               type: "clean",
-               padding: 10,
-               rows: [
-                  {
-                     cols: [
-                        {
-                           view: "label",
-                           label: L("DOCX file:"),
-                           css: "ab-text-bold",
-                           width: this.AB.UISettings.config().labelWidthXLarge,
-                        },
-                        {
-                           view: "uploader",
-                           value: L("Upload"),
-                           name: "docxFile",
-                           apiOnly: true,
-                           inputName: "file",
-                           multiple: false,
-                           on: {
-                              onBeforeFileAdd: (item) => {
-                                 return _logic.validateType(item);
-                              },
-
-                              onFileUpload: (file, response) => {
-                                 _logic.uploadedFile(file);
-                              },
-
-                              onFileUploadError: (file, response) => {},
-                           },
-                        },
-                     ],
-                  },
-                  {
-                     name: "filelabel",
-                     view: "text",
-                     label: L("Filename"),
-                     labelWidth: this.AB.UISettings.config().labelWidthLarge,
-                  },
-                  {
-                     name: "docxDownload",
-                     view: "button",
-                     type: "icon",
-                     css: "webix_primary",
-                     icon: "fa fa-file-word-o",
-                     label: L("Download Template File"),
-                     click: () => {
-                        _logic.downloadFile();
-                     },
-                  },
-               ],
-            },
-         },
-
-         {
-            view: "fieldset",
-            label: L("Language:"),
-            labelWidth: this.AB.UISettings.config().labelWidthLarge,
-            body: {
-               type: "clean",
-               padding: 10,
-               rows: [
-                  {
-                     name: "language",
-                     view: "richselect",
-                     label: L("Language"),
-                     labelWidth: this.AB.UISettings.config().labelWidthLarge,
-                  },
-               ],
-            },
-         },
-
-         {
-            view: "fieldset",
-            label: L("Customize Display:"),
-            labelWidth: this.AB.UISettings.config().labelWidthLarge,
-            body: {
-               type: "clean",
-               padding: 10,
-               rows: [
-                  {
-                     name: "buttonlabel",
-                     view: "text",
-                     label: L("Label"),
-                     labelWidth: this.AB.UISettings.config().labelWidthXLarge,
-                  },
-
-                  {
-                     view: "counter",
-                     name: "width",
-                     label: L("Width:"),
-                     labelWidth: this.AB.UISettings.config().labelWidthXLarge,
-                  },
-                  {
-                     view: "richselect",
-                     name: "toolbarBackground",
-                     label: L("Page background:"),
-                     labelWidth: this.AB.UISettings.config().labelWidthXLarge,
-                     options: [
-                        {
-                           id: "ab-background-default",
-                           value: L("White (default)"),
-                        },
-                        {
-                           id: "webix_dark",
-                           value: L("Dark"),
-                        },
-                        {
-                           id: "ab-background-lightgray",
-                           value: L("Gray"),
-                        },
-                     ],
-                  },
-
-                  {
-                     view: "richselect",
-                     name: "buttonPosition",
-                     label: L("Button Position:"),
-                     labelWidth: this.AB.UISettings.config().labelWidthXLarge,
-                     options: [
-                        {
-                           id: "left",
-                           value: L("Left (default)"),
-                        },
-                        {
-                           id: "center",
-                           value: L("Centered"),
-                        },
-                        {
-                           id: "right",
-                           value: L("Right"),
-                        },
-                     ],
-                  },
-               ],
-            },
-         },
-      ]);
-   }
-
-   static propertyEditorPopulate(App, ids, view) {
-      super.propertyEditorPopulate(App, ids, view);
-
-      let $DcSelector = $$(ids.datacollection);
-
-      let selectedDvId = view.settings.dataviewID
-         ? view.settings.dataviewID
-         : null;
-
-      $$(ids.toolbarBackground).setValue(
-         view.settings.toolbarBackground ||
-            ABViewDocxBuilderPropertyComponentDefaults.toolbarBackground
-      );
-      $$(ids.buttonPosition).setValue(
-         view.settings.buttonPosition ||
-            ABViewDocxBuilderPropertyComponentDefaults.buttonPosition
-      );
-
-      // Pull data views to options
-      let dcOptions = view.AB.datacollections().map((dc) => {
-         return {
-            id: dc.id,
-            value: dc.label,
-         };
-      });
-
-      $DcSelector.define("options", dcOptions);
-      $DcSelector.define("value", selectedDvId);
-      $DcSelector.refresh();
-
-      $$(ids.language).setValue(
-         view.settings.language ||
-            ABViewDocxBuilderPropertyComponentDefaults.language
-      );
-
-      $$(ids.filelabel).setValue(view.filelabel || view.settings.filelabel);
-      $$(ids.buttonlabel).setValue(
-         view.buttonlabel || view.settings.buttonlabel
-      );
-      $$(ids.width).setValue(view.settings.width);
-
-      if (view.settings.filename) {
-         $$(ids.docxDownload).show();
-      } else {
-         $$(ids.docxDownload).hide();
-      }
-   }
-
-   static propertyEditorValues(ids, view) {
-      super.propertyEditorValues(ids, view);
-
-      view.buttonlabel = $$(ids.buttonlabel).getValue();
-      view.settings.dataviewID = $$(ids.datacollection).getValue();
-      view.settings.width = $$(ids.width).getValue();
-      view.filelabel = $$(ids.filelabel).getValue();
-      view.settings.language = $$(ids.language).getValue();
-      view.settings.toolbarBackground = $$(ids.toolbarBackground).getValue();
-      view.settings.buttonPosition = $$(ids.buttonPosition).getValue();
-   }
-
    /**
     * @function component()
     * return a UI component based upon this view.
-    * @param {obj} App
+    * @param {obj} v1App
     * @return {obj} UI component
     */
-   component(App) {
+   component(v1App) {
+      let component = new ABViewDocxBuilderComponent(this);
+
+      // if this is our v1Interface
+      if (v1App) {
+         const newComponent = component;
+         component = {
+            ui: component.ui(),
+            init: (options, accessLevel) => {
+               return newComponent.init(this.AB, accessLevel);
+            },
+            onShow: (...params) => {
+               return newComponent.onShow?.(...params);
+            },
+         };
+      }
+
+      return component;
+   }
+
+   componentOld(App) {
       let baseCom = super.component(App);
 
-      var idBase = "ABViewDocxBuilder_" + this.id;
-      var ids = {
+      const idBase = `ABViewDocxBuilder_${this.id}`;
+      const ids = {
          button: App.unique(`${idBase}_button`),
          noFile: App.unique(`${idBase}_noFile`),
       };
 
-      var autowidth = false;
-      var buttonWidth =
-         this.settings.width ||
+      let autowidth = false;
+      let buttonWidth =
+         this.settings.width ??
          ABViewDocxBuilderPropertyComponentDefaults.width;
       if (buttonWidth == 0) {
          autowidth = true;
       }
 
-      var leftSpacer = {
+      let leftSpacer = {
          type: "spacer",
          width: 1,
       };
-      var rightSpacer = {
+      let rightSpacer = {
          type: "spacer",
          width: 1,
       };
-      var buttonPos =
-         this.settings.buttonPosition ||
+      const buttonPos =
+         this.settings.buttonPosition ??
          ABViewDocxBuilderPropertyComponentDefaults.buttonPosition;
       if (buttonPos == "left") {
          rightSpacer = {
@@ -394,13 +86,13 @@ module.exports = class ABViewDocxBuilder extends ABViewDocxBuilderCore {
          };
       }
 
-      var buttonLabelText = this.buttonlabel || this.settings.buttonlabel;
+      let buttonLabelText = this.buttonlabel || this.settings.buttonlabel; // Use || to check empty string ""
       if (!buttonLabelText) {
          buttonLabelText =
             ABViewDocxBuilderPropertyComponentDefaults.buttonlabel;
       }
 
-      var _ui = {
+      const _ui = {
          view: "toolbar",
          css:
             this.settings.toolbarBackground ||
@@ -1105,7 +797,7 @@ module.exports = class ABViewDocxBuilder extends ABViewDocxBuilderCore {
                .then((blobFile) => {
                   // Let user download the output file
                   return new Promise((next, err) => {
-                     letUserDownload(blobFile, this.filelabel);
+                     this.letUserDownload(blobFile, this.filelabel);
 
                      next();
                   });
