396c395e37336a8ce1279d34283c5c39eabbb133
Nate <7259@pm.me>
2022-08-02 16:57:34 +0700

    cleanup npm dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CompressionPlugin = require("compression-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin")
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
    mode: "production",
