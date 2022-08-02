9af61d7eb01ab07f4f43f954cf42e85b2acda412
Nate <7259@pm.me>
2022-08-02 16:52:29 +0700

    optimize webpack for caching assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,13 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CompressionPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
    mode: "production",
-   plugins: [new CompressionPlugin()],
+   plugins: [new CompressionPlugin({
+      exclude: /index\.ejs/
+   })],
+   devtool: "source-map",
 });
