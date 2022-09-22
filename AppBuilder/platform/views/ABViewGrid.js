ca904b7a680d2ff3ebd63eb3de76392ad9a6f52c
James Duncan <james@digiserve.org>
2022-09-22 13:14:13 -0500

    allow user to clear combo boxes in editable grids

--- AppBuilder/platform/views/ABViewGrid.js
+++ AppBuilder/platform/views/ABViewGrid.js
@@ -5,8 +5,8 @@ import ABPopupExport from "./ABViewGridPopupExport";
 import ABPopupMassUpdateClass from "./ABViewGridPopupMassUpdate";
 import ABPopupSortField from "./ABViewGridPopupSortFields";
 import ABViewGridFilter from "./viewProperties/ABViewPropertyFilterData";
-const ABViewPropertyLinkPage = require("./viewProperties/ABViewPropertyLinkPage")
-   .default;
+const ABViewPropertyLinkPage =
+   require("./viewProperties/ABViewPropertyLinkPage").default;
 
 const KEY_STORAGE_SETTINGS = "abviewgrid_settings";
 // {string}
@@ -574,8 +574,8 @@ class ABViewGridComponent extends ABViewComponent {
 
          let verticalScrollState = DataTable.getScrollState().y,
             rowHeight = DataTable.config.rowHeight,
-            height = DataTable.$view.querySelector(".webix_ss_body")
-               .clientHeight,
+            height =
+               DataTable.$view.querySelector(".webix_ss_body").clientHeight,
             startRecIndex = Math.floor(verticalScrollState / rowHeight),
             endRecIndex = startRecIndex + DataTable.getVisibleCount(),
             index = 0;
@@ -662,6 +662,22 @@ class ABViewGridComponent extends ABViewComponent {
          } else if (e.target.className.indexOf("track") > -1) {
             self.emit("object.track", CurrentObject, id.row);
             // App.actions.openObjectTrack(CurrentObject, id.row);
+         } else if (e.target.className.indexOf("clear-combo-value") > -1) {
+            let clearValue = {};
+            clearValue[id.column] = "";
+            CurrentObject.model()
+               .update(id.row, clearValue)
+               .then((response) => {
+                  // console.log(response);
+               })
+               .catch((err) => {
+                  self.AB.notify.developer(err, {
+                     context: "ABViewGridComponent.onItemClick",
+                     message: "Error updating item",
+                     obj: CurrentObject.toObj(),
+                     id: id.row,
+                  });
+               });
          } else if (e.target.className.indexOf("trash") > -1) {
             // if this was our trash icon:
 
