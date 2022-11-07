6d48b6347e400f589328a8b77abd90029a1f3dcd
wongpraatan <wongpratan@hotmail.com>
2022-11-07 20:17:09 +0700

    +Add show/hide the save button in filterComplex - https://team.digiserve.org/main/pl/s781fncgyp8pim1uhyswxis11c

--- AppBuilder/platform/FilterComplex.js
+++ AppBuilder/platform/FilterComplex.js
@@ -31,7 +31,9 @@ function _toInternal(cond, fields = []) {
       //       },
       //    ],
       // }
-      let field = fields.filter((f) => f.id == cond.key)[0];
+      const field = fields.filter(
+         (f) => f.id == cond.key || f.columnName == cond.key
+      )[0];
       cond.field = field?.columnName ?? field?.id;
       cond.condition = {
          type: cond.rule,
@@ -109,10 +111,12 @@ function _toExternal(cond, fields = []) {
 }
 
 module.exports = class FilterComplex extends FilterComplexCore {
-   constructor(idBase, AB) {
+   constructor(idBase, AB, options = {}) {
       idBase = idBase ?? "ab_filterComplex";
 
       super(idBase, AB);
+
+      this._options = options ?? {};
 
       this._initComplete = false;
       // {bool}
@@ -226,6 +230,7 @@ module.exports = class FilterComplex extends FilterComplexCore {
                view: "button",
                css: "webix_primary",
                value: L("Save"),
+               hidden: this._options.isSaveHidden ?? false,
                click: () => {
                   if (this.myPopup) this.myPopup.hide();
                   this.emit("save", this.getValue());
@@ -584,7 +589,7 @@ module.exports = class FilterComplex extends FilterComplexCore {
                      this.queryView(function (a) {
                         return !a.getParentView();
                      }, "parent") ?? this;
-                  $layout.$view.style.zIndex = 102;
+                  $layout.$view.style.zIndex = 202;
                },
             },
          },
@@ -751,8 +756,10 @@ module.exports = class FilterComplex extends FilterComplexCore {
    }
 
    uiCustomValue(field) {
-      let customOptions = this._customOptions ?? {};
-      let options = customOptions[field.id ?? field] ?? {};
+      if (!field) return [];
+
+      const customOptions = this._customOptions ?? {};
+      const options = customOptions[field.id ?? field] ?? {};
       return options.values ?? [];
    }
 
