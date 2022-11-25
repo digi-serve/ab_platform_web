8d4bec7477c6d33d1e7eefc3871e892ed03d7ebb
nohrsom <sky_wyvern@hotmail.com>
2022-11-25 15:53:36 +0700

    Fix bug widget Chart

--- AppBuilder/platform/views/ABViewChart.js
+++ AppBuilder/platform/views/ABViewChart.js
@@ -51,16 +51,22 @@ module.exports = class ABViewChart extends ABViewChartCore {
 
    refreshData() {
       const dc = this.datacollection;
-
-      if (!dc) return;
+      if (dc == null) return this.dcChart;
 
       const labelCol = this.labelField();
+      const valueCol = this.valueField();
+      const valueCol2 = this.valueField2();
 
-      if (!labelCol) return;
+      if (!labelCol || !valueCol) return this.dcChart;
 
-      const columnName = this.valueField()?.columnName;
+      // const labelColName = labelCol.columnName;
+      const numberColName = valueCol.columnName;
 
-      if (!columnName) return;
+      let numberColName2 = "";
+
+      if (this.settings.multipleSeries && valueCol2) {
+         numberColName2 = valueCol2.columnName;
+      }
 
       const colorList = [
          "#ee4339",
@@ -74,57 +80,213 @@ module.exports = class ABViewChart extends ABViewChartCore {
          "#a244ea",
          "#e33fc7",
       ];
+
       const dInfo = dc.getData();
-      const results = [];
 
-      dInfo.forEach((e, i) => {
-         const label = labelCol.isConnection
-            ? labelCol
-                 .pullRelationValues(e)
-                 .map?.((e) => e.text || "")
-                 .join(", ") ?? labelCol.pullRelationValues(e).text
-            : labelCol.format(e);
-         const data = {};
+      let results = [];
+      let sumData = {};
+      let sumNumber = 0;
+      let sumNumber2 = 0;
+      let countNumber = dInfo.length;
 
-         data.label = label;
-         data.color = colorList[i % colorList.length];
-         data.value = parseFloat(e[columnName] ?? 0);
+      dInfo.forEach((item) => {
+         const labelKey = labelCol.format(item) || item.id;
 
+         let numberVal = parseFloat(item[numberColName] || 0);
+         let numberVal2 = null;
+
+         if (this.settings.multipleSeries)
+            numberVal2 = parseFloat(item[numberColName2]) || 0;
+
+         switch (valueCol.key) {
+            //Formula Datatype
+            case "formula":
+               numberVal = valueCol.format(item);
+
+               break;
+
+            //Calcualte Datatype
+            case "calculate":
+               numberVal = parseFloat(
+                  valueCol.constructor.convertToJs(
+                     valueCol.object,
+                     valueCol.settings.formula,
+                     item,
+                     valueCol.settings.decimalPlaces
+                  )
+               );
+
+               break;
+
+            default:
+               break;
+         }
+
+         if (sumData[labelKey] == null) {
+            let label = labelKey;
+
+            // Get label of the connect field
+            if (labelCol.isConnection) {
+               let relateValues = labelCol.pullRelationValues(item);
+               if (relateValues != null) {
+                  if (Array.isArray(relateValues))
+                     label = relateValues
+                        .map((val) => val.text || "")
+                        .join(", ");
+                  else label = relateValues.text;
+               }
+            }
+
+            if (this.settings.multipleSeries) {
+               sumData[labelKey] = {
+                  label: label || item.id,
+                  value: 0,
+                  value2: 0,
+               };
+            } else {
+               sumData[labelKey] = {
+                  label: label || item.id,
+                  value: 0,
+               };
+            }
+         }
+
+         sumData[labelKey].value += numberVal;
+         sumNumber += numberVal;
+
+         if (this.settings.multipleSeries) {
+            sumData[labelKey].value2 += numberVal2;
+            sumNumber2 += numberVal2;
+         }
+      });
+
+      let index = 0;
+
+      for (const key in sumData) {
+         let val = sumData[key].value;
+
+         if (val <= 0) continue;
+
+         // Display to percent values
          if (this.settings.isPercentage) {
-            data.value = data.value
-               ? // round decimal 2 digits
-                 Math.round(
-                    (data.value /
-                       dInfo.reduce((sum, e) => sum + e[columnName], 0)) *
-                       10000
-                 ) / 100
-               : 0;
+            val = (val / sumNumber) * 100;
+            val = Math.round(val * 100) / 100; // round decimal 2 digits
+            val = val + " %";
          }
 
          if (this.settings.multipleSeries) {
-            const columnName2 = this.valueField2()?.columnName;
+            let val2 = sumData[key].value2;
 
-            data.value2 = parseFloat(e[columnName2] ?? 0);
+            if (val2 <= 0) continue;
 
-            if (this.settings.isPercentage)
-               data.value2 = data.value2
-                  ? // round decimal 2 digits
-                    Math.round(
-                       (data.value2 /
-                          dInfo.reduce((sum, e) => sum + e[columnName2], 0)) *
-                          10000
-                    ) / 100
-                  : 0;
+            // Display to percent values
+            if (this.settings.isPercentage) {
+               val2 = (val2 / sumNumber2) * 100;
+               val2 = Math.round(val2 * 100) / 100; // round decimal 2 digits
+               val2 = val2 + " %";
+            }
+
+            results.push({
+               label: sumData[key].label,
+               value: val,
+               value2: val2,
+               color: colorList[index % colorList.length],
+               count: countNumber,
+            });
+         } else {
+            results.push({
+               label: sumData[key].label,
+               value: val,
+               color: colorList[index % colorList.length],
+               count: countNumber,
+            });
          }
 
-         results.push(data);
-      });
+         index += 1;
+      }
 
       const dcChart = this.getDCChart();
 
       dcChart.clearAll();
       dcChart.parse(results);
    }
+
+   // refreshData() {
+   //    const dc = this.datacollection;
+
+   //    if (!dc) return;
+
+   //    const labelCol = this.labelField();
+
+   //    if (!labelCol) return;
+
+   //    const columnName = this.valueField()?.columnName;
+
+   //    if (!columnName) return;
+
+   //    const colorList = [
+   //       "#ee4339",
+   //       "#ee9336",
+   //       "#eed236",
+   //       "#d3ee36",
+   //       "#a7ee70",
+   //       "#58dccd",
+   //       "#36abee",
+   //       "#476cee",
+   //       "#a244ea",
+   //       "#e33fc7",
+   //    ];
+   //    const dInfo = dc.getData();
+   //    const results = [];
+
+   //    dInfo.forEach((e, i) => {
+   //       const label = labelCol.isConnection
+   //          ? labelCol
+   //               .pullRelationValues(e)
+   //               .map?.((e) => e.text || "")
+   //               .join(", ") ?? labelCol.pullRelationValues(e).text
+   //          : labelCol.format(e);
+   //       const data = {};
+
+   //       data.label = label;
+   //       data.color = colorList[i % colorList.length];
+   //       data.value = parseFloat(e[columnName] ?? 0);
+
+   //       if (this.settings.isPercentage) {
+   //          data.value = data.value
+   //             ? // round decimal 2 digits
+   //               Math.round(
+   //                  (data.value /
+   //                     dInfo.reduce((sum, e) => sum + e[columnName], 0)) *
+   //                     10000
+   //               ) / 100
+   //             : 0;
+   //       }
+
+   //       if (this.settings.multipleSeries) {
+   //          const columnName2 = this.valueField2()?.columnName;
+
+   //          data.value2 = parseFloat(e[columnName2] ?? 0);
+
+   //          if (this.settings.isPercentage)
+   //             data.value2 = data.value2
+   //                ? // round decimal 2 digits
+   //                  Math.round(
+   //                     (data.value2 /
+   //                        dInfo.reduce((sum, e) => sum + e[columnName2], 0)) *
+   //                        10000
+   //                  ) / 100
+   //                : 0;
+   //       }
+
+   //       results.push(data);
+   //    });
+
+   //    const dcChart = this.getDCChart();
+
+   //    dcChart.clearAll();
+   //    dcChart.parse(results);
+   // }
 
    componentOld() {}
 };
