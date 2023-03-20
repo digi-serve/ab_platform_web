1a745b4602dcfc2e3072e9cd6bbc90b2546a970e
James Duncan <james@digiserve.org>
2023-03-20 15:37:51 +0700

    ensure proper values are set before looking up component.

--- AppBuilder/platform/views/viewComponent/ABViewFormJsonComponent.js
+++ AppBuilder/platform/views/viewComponent/ABViewFormJsonComponent.js
@@ -17,7 +17,10 @@ module.exports = class ABViewFormJsonComponent extends ABViewFormItemComponent {
    }
 
    getFilterField(instance) {
-      if (instance.settings.filterField) {
+      if (
+         instance?.settings?.filterField &&
+         instance?.view?.parent?.viewComponents
+      ) {
          let filterField = "";
          for (const [key, value] of Object.entries(
             instance.view.parent.viewComponents
