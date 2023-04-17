bb5810648f6dc46ba89fdfa43ba6c5e44ba2d746
wongpraatan <wongpratan@hotmail.com>
2023-04-17 23:05:45 +0700

    *[hotfix] return component of the bar chart widget

--- AppBuilder/platform/views/ABViewChartBar.js
+++ AppBuilder/platform/views/ABViewChartBar.js
@@ -12,6 +12,6 @@ module.exports = class ABViewChartBar extends ABViewChartBarCore {
     * @return {obj} UI component
     */
    component() {
-      let component = new ABViewChartBarComponent(this);
+      return new ABViewChartBarComponent(this);
    }
 };
