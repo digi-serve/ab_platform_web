3854ada39e641ef89e44b19cc2fc3e8f99d70f94
Nate <7259@pm.me>
2023-09-07 11:49:06 +0700

    inject the webpack mode to use as sentry environment

--- init/sentry.js
+++ init/sentry.js
@@ -1,21 +1,13 @@
 import * as Sentry from "@sentry/browser";
-
 Sentry.init({
    dsn: "https://c16443e39a66eae141954dfd23890812@o144358.ingest.sentry.io/4505832903147520",
+   environment: WEBPACK_MODE,
+   /* global WEBPACK_MODE - This is set by the DefinePlugin in webpack. */
 
-   // Alternatively, use `process.env.npm_package_version` for a dynamic release version
-   // if your build tool supports it.
    //    release: "my-project-name@2.3.12",
    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
    normalizeDepth: 5,
-
-   // Set tracesSampleRate to 1.0 to capture 100%
-   // of transactions for performance monitoring.
-   // We recommend adjusting this value in production
    tracesSampleRate: 0.1,
-
-   // Capture Replay for 10% of all sessions,
-   // plus for 100% of sessions with an error
    replaysSessionSampleRate: 0,
    replaysOnErrorSampleRate: 0.1,
 });
