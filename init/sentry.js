5349c70a9f2ef7e40ffb07611c239620de63e355
Nate <7259@pm.me>
2023-09-18 13:11:36 +0700

    fix catch if WEBPACK_MODE  variable not found

--- init/sentry.js
+++ init/sentry.js
@@ -1,7 +1,13 @@
 import * as Sentry from "@sentry/browser";
+let webpackMode = "develop";
+try {
+   webpackMode = WEBPACK_MODE;
+} catch (err) {
+   console.warn("WEBPACK_MODE variable not found, falling back to develop");
+}
 Sentry.init({
    dsn: "https://c16443e39a66eae141954dfd23890812@o144358.ingest.sentry.io/4505832903147520",
-   environment: WEBPACK_MODE,
+   environment: webpackMode,
    /* global WEBPACK_MODE - This is set by the DefinePlugin in webpack. */
 
    //    release: "my-project-name@2.3.12",
