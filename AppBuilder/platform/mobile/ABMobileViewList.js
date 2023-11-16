ba9c478699a126fc168cb2517fa886d1f321b0b8
Johnny Hausman <johnny@hausmanfamily.com>
2023-11-15 18:01:07 -0600

    [add] Mobile List widget

---
+++ AppBuilder/platform/mobile/ABMobileViewList.js
@@ -0,0 +1,13 @@
+const ABMobileViewListCore = require("../../core/mobile/ABMobileViewListCore");
+
+module.exports = class ABMobileViewList extends ABMobileViewListCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+   // warningsEval() {
+   //    super.warningsEval();
+   //    if (!this.text) {
+   //       this.warningsMessage("has no text value set.");
+   //    }
+   // }
+};
