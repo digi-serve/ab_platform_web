e8bc4113ea09b68ec48bf8bbf723d7d583946501
Nate <7259@pm.me>
2023-12-20 14:20:08 +0700

    fixes

--- ciLighthouseAuth.js
+++ ciLighthouseAuth.js
@@ -2,17 +2,32 @@
  * @param {puppeteer.Browser} browser
  * @param {{url: string, options: LHCI.CollectCommand.Options}} context
  */
-module.exports = async (browser, context) => {
+module.exports = async (browser) => {
    // launch browser for LHCI
+   console.log("AppBuilder Auth - Start");
    const page = await browser.newPage();
-   await page.goto("http://localhost");
-   await page.type(
-      '[data-cy="portal_auth_login_form_email"]',
-      "admin@email.com"
-   );
-   await page.type('[data-cy="portal_auth_login_form_password"]', "admin");
-   await page.click('[data-cy="portal_auth_login_form_submit"]');
-   await page.waitForNavigation();
+   page.on("console", (message) => {
+      console.log(`Console: ${message.text()}`);
+   });
+   page.on("error", (error) => {
+      console.log("Error in page context:", error);
+   });
+   try {
+      await page.setDefaultNavigationTimeout(90000);
+      await page.goto("http://127.0.0.1");
+      await page.waitForSelector('[data-cy="portal_auth_login_form_email"]');
+      await page.type(
+         '[data-cy="portal_auth_login_form_email"]',
+         "neo@thematrix.com"
+      );
+      await page.type('[data-cy="portal_auth_login_form_password"]', "admin");
+      await page.click('[data-cy="portal_auth_login_form_submit"]');
+      await page.waitForNavigation();
+   } catch (err) {
+      console.log("Error in puppeteer auth script:", err);
+   }
+
    // close session for next run
    await page.close();
+   console.log("AppBuilder Auth - Finished");
 };
