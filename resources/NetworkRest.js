7b9a916a3ff86cdb67e4a9123dbbb2992e28f2e4
Nate <7259@pm.me>
2023-12-22 11:29:00 +0700

    Fix object captured as error
    [APPBUILDER-WEB-XV](https://appdev-designs.sentry.io/issues/4514863163/)

--- resources/NetworkRest.js
+++ resources/NetworkRest.js
@@ -279,7 +279,8 @@ class NetworkRest extends EventEmitter {
                         })
                         .catch((_err) => {
                            this.AB.notify.developer(_err, {
-                              message: "*** NetworkRest._request().catch(): retry failed:",
+                              message:
+                                 "*** NetworkRest._request().catch(): retry failed:",
                            });
                            reject(_err);
                         });
@@ -311,10 +312,11 @@ class NetworkRest extends EventEmitter {
                         return;
                      }
 
-                     this.AB.notify.developer(packet, {
+                     this.AB.notify.developer(new Error(packet.message), {
                         context:
                            "NetworkRest:_request:Error returned from Server (req.ab.error())",
                         data: packet.data,
+                        status: packet.status,
                      });
                      // this.AB.Analytics.logError(packet.data);
                      // this.AB.error(packet.data);
