7385b202628c828d6db15f59754708afcf6b4f1b
Johnny <johnny@digiserve.org>
2024-04-30 15:14:55 -0500

    [fix] case where ABViewDocXBuilder returns an array of datacollections

--- AppBuilder/platform/views/viewComponent/ABViewComponent.js
+++ AppBuilder/platform/views/viewComponent/ABViewComponent.js
@@ -182,8 +182,17 @@ export default class ABViewComponent extends ClassUI {
 
       if (!dc) return;
 
-      if (dc.dataStatus === dc.dataStatusFlag.notInitial)
-         // load data when a widget is showing
-         dc.loadData();
+      // ABViewDocXBuilderComponents can send back [dc,...]
+      if (!Array.isArray(dc)) {
+         if (dc.dataStatus === dc.dataStatusFlag.notInitial)
+            // load data when a widget is showing
+            dc.loadData();
+      } else {
+         dc.forEach((d) => {
+            if (d.dataStatus === d.dataStatusFlag.notInitial)
+               // load data when a widget is showing
+               d.loadData();
+         });
+      }
    }
 }
