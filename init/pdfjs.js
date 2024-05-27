d16ee04623a581565cc7ee9de307923f6ef91a60
wongpratan <wongpratan@hotmail.com>
2024-05-27 17:13:23 +0700

    *Fix import pdf.js

--- init/pdfjs.js
+++ init/pdfjs.js
@@ -4,8 +4,16 @@
  * size of the app.
  */
 
-import "pdfjs-dist/build/pdf.worker";
+import "pdfjs-dist/build/pdf.worker.mjs";
 // import pdfjs from "pdfjs-dist/webpack";
-const pdfjs = require("pdfjs-dist/webpack");
+const pdfjs = require("pdfjs-dist/webpack.mjs");
 
 export default pdfjs;
+
+// import * as pdfjsLib from "pdfjs-dist";
+// import * as pdfWorker from "pdfjs-dist/build/pdf.worker.mjs";
+
+// // Setting worker path to worker bundle.
+// pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker;
+
+// export { pdfjsLib };
